08a538888c8e92e741fea112b33b6bedfd54eefc
nahkd123 <nahkd123@gmail.com>
2025-07-19 18:35:37 +0700

    TIFF Explorer: Update name list with some DNG fields, add some hints

--- lab/tiff-explorer/main.ts
+++ lab/tiff-explorer/main.ts
@@ -1,8 +1,5 @@
-import { header, ifd } from "./mod.ts";
+import { decode as decodeTiff } from "./mod.ts";
 
-const sample = await Deno.readFile(new URL(import.meta.resolve("./sample.nef")));
+const sample = await Deno.readFile(new URL(import.meta.resolve("./sample.dng")));
 const file = new Blob([sample]);
-
-const head = await header.decode(file);
-const entries = await ifd.decode(file, head.ifdOffset, head.endianness);
-console.log(entries);
+console.log(await decodeTiff(file));
